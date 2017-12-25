2935bed636443300b25dd85becc68058670bdcce
Janus <janus.lin@tripresso.com>
2017-12-25 18:09:11 +0800

    [Add] sidebar & footer

--- application/views/layout/adminLTE/Page.php
+++ application/views/layout/adminLTE/Page.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
 <html lang="zh_tw">
-<!-- header -->
+<!-- header (loading js & css before) -->
 <?php
 $this->load->view('layout/adminLTE/header');
 ?>
-<body class="skin-blue-light sidebar-mini sidebar-collapse">
+<body class="skin-blue sidebar-mini sidebar-collapse">
     <div class="wrapper" style="min-height:183px">
         <!-- head bar -->
         <?php $this->load->view('layout/adminLTE/headBar') ?>
 
         <!-- side menu -->
-        <?php ?>
+        <?php $this->load->view('layout/adminLTE/sideBarLeft') ?>
 
         <!-- main content -->
         <div class="content-wrapper row">
             <?php echo $viewContent; ?>
         </div>
+        <!-- footer (loading js & css afterward) -->
+        <?php $this->load->view('layout/adminLTE/footer'); ?>
     </div>
-    <!-- footer -->
-    <?php
-    $this->load->view('layout/adminLTE/footer');
-    ?>
 </body>
 </html>
--- application/views/layout/adminLTE/footer.php
+++ application/views/layout/adminLTE/footer.php
@@ -1,3 +1,10 @@
+<footer class="main-footer row">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.0.1
+    </div>
+    <strong>Copyright © 2017 <a href="https://github.com/j4nusl1n/Credit-Card-System" target="_blank"><i class="fa fa-github"></i> JanusLin</a>.</strong> All rights
+    reserved.
+</footer>
 <div>
 <?php echo $this->load->setJs(); ?>
 </div>
