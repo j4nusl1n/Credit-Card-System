5568077f5ec5be2921e77a1381d671a6e25b3397
Janus <janus.lin@tripresso.com>
2017-12-28 17:21:13 +0800

    [Chg] model class rename

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class auth_model extends MY_Model {
+class Auth_model extends MY_Model {
     
     public function __construct() {
         parent::__construct();
