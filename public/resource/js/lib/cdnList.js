5cff7c2701b68a1c9f440f4b5f8c0c6b81557880
Janus <janus.lin@tripresso.com>
2017-12-29 15:58:28 +0800

    [Chg] Change AdminLTE js version to 2.3.8

--- public/resource/js/lib/cdnList.js
+++ public/resource/js/lib/cdnList.js
@@ -2,6 +2,6 @@ var CDN = {
     jquery: ['lib/vendor/jQuery/3.2.1/jquery.min', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min'],
     underscore: ['lib/vendor/underscore/1.8.3/underscore-min', 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min'],
     backbone: ['lib/vendor/backbone/1.3.3/backbone-min', 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min'],
-    adminLTE: ['lib/vendor/adminLTE/adminlte.min', 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.2/js/adminlte.min'],
+    adminLTE: ['lib/vendor/adminLTE/app.min', 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/js/app.min'],
     bootstrap: ['lib/vendor/bootstrap/3.3.7/bootstrap.min', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min'],
 };
